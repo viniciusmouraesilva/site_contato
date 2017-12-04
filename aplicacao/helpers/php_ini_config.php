e6c46405721a2a975fccb4ab8c1e883da39447a7
viniciusmouraesilva <viniciusmouraesilva2204@gmail.com>
2017-12-04 12:21:04 -0200

    configurações do php ini

--- aplicacao/helpers/php_ini_config.php
+++ aplicacao/helpers/php_ini_config.php
@@ -5,6 +5,8 @@ ini_set('allow_url_include','0');
 ini_set('cgi.force_redirect','1');
 ini_set('date.timezone','America/Sao_Paulo');
 ini_set('display_erros','1'); #produção é 0
+ini_set('display_startup_errors','1'); //produção é 0
+//error_reporting(0); produção é 0
 ini_set('expose_php','0');
 #ini_set('memory_limit','16');
 ini_set('doc_root','public_html');
