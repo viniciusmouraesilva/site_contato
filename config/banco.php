9604261b6a48baeb14d6a20a5a318c8524e67bdd
viniciusmouraesilva <viniciusmouraesilva2204@gmail.com>
2017-12-10 20:17:31 -0200

    caracteres do banco charset

--- config/banco.php
+++ config/banco.php
@@ -1,5 +1,5 @@
 <?php
 #constantes para conexão
-define("DSN","mysql:dbname=portfolio;host=localhost;utf8mb4");
+define("DSN","mysql:dbname=portfolio;host=localhost;charset=utf8mb4");
 define("USUARIO","root");
 define("SENHA","");
