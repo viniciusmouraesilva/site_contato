64e88322070358c6b68b6eaa39c877c34d55f565
Vinícius Moura <30481009+viniciusmouraesilva@users.noreply.github.com>
2018-01-23 15:11:45 -0200

    Update banco_login.php

--- config/banco_login.php
+++ config/banco_login.php
@@ -1,5 +1,5 @@
 <?php
 #usuario diferente para quem efetua login
-define("DSN","mysql:dbname=portfolio;host=localhost;utf8mb4");
+define("DSN","mysql:dbname=portfolio;host=localhost;charset=utf8mb4");
 define("USER","root");
 define("SENHA","");
